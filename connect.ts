7417c900733fce16d18fe7059682860d310df264
Nicolas Demontrond <demontrond.nicolas@gmail.com>
2025-01-14 18:58:00 +0100

    Update connect.ts

--- connect.ts
+++ connect.ts
@@ -1,4 +1,10 @@
 import fs from 'fs'
+import path from 'path'
 
-const caCert = fs.readFileSync(process.cwd(), 'ca.pem')
-const connectionString = `postgres://username:password@hostname:port/database?sslmode=require&sslrootcert=ca.pem`
+// Combine the current working directory and the relative path to 'ca.pem'
+const caCertPath = path.join(process.cwd(), 'ca.pem')
+
+// Read the certificate file
+const caCert = fs.readFileSync(caCertPath)
+
+console.log('Resolved path:', caCertPath)
