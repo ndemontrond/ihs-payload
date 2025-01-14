79ddd63ccde55866f44b5df2795be07025d8990d
Nicolas Demontrond <demontrond.nicolas@gmail.com>
2025-01-14 18:55:29 +0100

    change

--- src/utilities/connect.ts
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-
-const caCert = fs.readFileSync('./ca.pem');
-const connectionString = `postgres://username:password@hostname:port/database?sslmode=require&sslrootcert=ca.pem`;
